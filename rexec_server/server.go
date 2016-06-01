69000909565212e50287268a29c97ddd7e6672e1
Christian Pointner <cpointner@mgit.at>
2016-06-01 21:54:27 +0200

    simplified version of rexec_server using sytemd socket activation with Accept=true

--- rexec_server/server.go
+++ rexec_server/server.go
@@ -7,9 +7,9 @@ import (
 	"os/exec"
 	"syscall"
 
+	"github.com/coreos/go-systemd/activation"
 	"github.com/docker/libchan"
 	"github.com/docker/libchan/spdy"
-	//	"github.com/coreos/go-systemd/activation"
 )
 
 // RemoteCommand is the received command parameters to execute locally and return
@@ -28,77 +28,61 @@ type CommandResponse struct {
 }
 
 func main() {
-	listener, err := net.Listen("unix", "/tmp/rexec")
+	files := activation.Files(true)
+	if len(files) != 1 {
+		log.Fatal("invalid number of sockets passed from systemd")
+	}
+
+	conn, err := net.FileConn(files[0])
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	for {
-		c, err := listener.Accept()
-		if err != nil {
-			log.Print(err)
-			break
+	p, err := spdy.NewSpdyStreamProvider(conn, true)
+	if err != nil {
+		log.Fatal(err)
+	}
+	t := spdy.NewTransport(p)
+
+	receiver, err := t.WaitReceiveChannel()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	command := &RemoteCommand{}
+	err = receiver.Receive(command)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := exec.Command(command.Cmd, command.Args...)
+	cmd.Stdout = command.Stdout
+	cmd.Stderr = command.Stderr
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		log.Fatal(err)
+	}
+	go func() {
+		io.Copy(stdin, command.Stdin)
+		stdin.Close()
+	}()
+
+	res := cmd.Run()
+	command.Stdout.Close()
+	command.Stderr.Close()
+	returnResult := &CommandResponse{}
+	if res != nil {
+		if exiterr, ok := res.(*exec.ExitError); ok {
+			returnResult.Status = exiterr.Sys().(syscall.WaitStatus).ExitStatus()
+		} else {
+			log.Print(res)
+			returnResult.Status = 10
 		}
-		p, err := spdy.NewSpdyStreamProvider(c, true)
-		if err != nil {
-			log.Print(err)
-			break
-		}
-		t := spdy.NewTransport(p)
+	}
 
-		go func() {
-			for {
-				receiver, err := t.WaitReceiveChannel()
-				if err != nil {
-					log.Print(err)
-					break
-				}
-
-				go func() {
-					for {
-						command := &RemoteCommand{}
-						err := receiver.Receive(command)
-						if err != nil {
-							if err != io.EOF {
-								log.Print(err)
-							}
-							break
-						}
-
-						cmd := exec.Command(command.Cmd, command.Args...)
-						cmd.Stdout = command.Stdout
-						cmd.Stderr = command.Stderr
-
-						stdin, err := cmd.StdinPipe()
-						if err != nil {
-							log.Print(err)
-							break
-						}
-						go func() {
-							io.Copy(stdin, command.Stdin)
-							stdin.Close()
-						}()
-
-						res := cmd.Run()
-						command.Stdout.Close()
-						command.Stderr.Close()
-						returnResult := &CommandResponse{}
-						if res != nil {
-							if exiterr, ok := res.(*exec.ExitError); ok {
-								returnResult.Status = exiterr.Sys().(syscall.WaitStatus).ExitStatus()
-							} else {
-								log.Print(res)
-								returnResult.Status = 10
-							}
-						}
-
-						err = command.StatusChan.Send(returnResult)
-						if err != nil {
-							log.Print(err)
-						}
-					}
-				}()
-			}
-		}()
+	err = command.StatusChan.Send(returnResult)
+	if err != nil {
+		log.Print(err)
 	}
 }
